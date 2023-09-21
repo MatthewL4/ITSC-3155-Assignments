c35d0ed8362e097cdc221c0359691952e3ef12aa
MatthewL4 <125594147+MatthewL4@users.noreply.github.com>
2023-09-21 12:55:26 -0400

    feat: add process_coins function

--- Assignment2/sandwich_maker.py
+++ Assignment2/sandwich_maker.py
@@ -6,11 +6,6 @@ class SandwichMaker:
     def check_resources(self, ingredients):
         """Returns True when order can be made, False if ingredients are insufficient."""
         #####
-        for ingredient, quantity in ingredients.items():
-            if self.machine_resources.get(ingredient) < quantity:
-                return False
-            return True
 
     def make_sandwich(self, sandwich_size, order_ingredients):
-        for ingredient, quantity in order_ingredients.items():
-            self.machine_resources[ingredient] -= quantity
+
