e5c3b056531a83e6377f1a41feffbe7746321a6e
MatthewL4 <125594147+MatthewL4@users.noreply.github.com>
2023-09-21 12:56:28 -0400

    feat: add check_resources function

--- Assignment2/sandwich_maker.py
+++ Assignment2/sandwich_maker.py
@@ -6,6 +6,10 @@ class SandwichMaker:
     def check_resources(self, ingredients):
         """Returns True when order can be made, False if ingredients are insufficient."""
         #####
+        for ingredient, quantity in ingredients.items():
+            if self.machine_resources.get(ingredient) < quantity:
+                return False
+            return True
 
     def make_sandwich(self, sandwich_size, order_ingredients):
 
