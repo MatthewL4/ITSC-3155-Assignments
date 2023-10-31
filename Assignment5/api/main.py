a50772cc13be71e1dad4ed327d34c5ead85a1d59
MatthewL4 <125594147+MatthewL4@users.noreply.github.com>
2023-10-31 16:04:29 -0400

    refactor: change post create name for sandwich

--- Assignment5/api/main.py
+++ Assignment5/api/main.py
@@ -45,7 +45,7 @@ def delete_one_order(order_id: int, db: Session = Depends(get_db)):
 
 
 @app.post("/sandwiches/", response_model=schemas.Sandwich, tags=["Sandwiches"])
-def make_sandwich(sandwich: schemas.SandwichCreate, db: Session = Depends(get_db)):
+def create_sandwich(sandwich: schemas.SandwichCreate, db: Session = Depends(get_db)):
     return sandwiches.create(db=db, sandwich=sandwich)
 
 
